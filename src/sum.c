5d26168614de0a42b7ba6785b0314666a225d1c2
Peter Laust Almvig <petealmvig@gmail.com>
2022-11-17 13:52:52 +0100

    sum tail recursion and sum while loop added.

--- src/sum.c
+++ src/sum.c
@@ -18,12 +18,21 @@ int sum (int n)
 /* Sum integers 1 to n */
 int sumtail (int n, int total)
 {
-    return 0;
+    if(n == 1){
+      return n + total;
+    }
+    else return sumtail(n-1, total + n);
 }
 
 /* Sum integers 1 to n */
 int sumwhile (int n)
 {
-  return 0;
+  int i = 1;
+  int sum = 0;
+  while(i <= n){
+    sum += i;
+    i++;
+  }
+  return sum;
 }
 
